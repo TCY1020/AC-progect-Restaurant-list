ec562c6fc7617405039073ea255071f0baa2107a
TCY1020 <a25023423@gmail.com>
2023-05-10 04:22:14 +0800

    feat: search from mongodb

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurants.json')
-
+const Restaurant = require('./models/Restaurant.js')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -29,36 +29,65 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'), bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index',{
-    restaurants: restaurantList.results
-  })
+  Restaurant.find()
+  .lean()
+  .then(restaurants => res.render('index', {restaurants}))
+  .catch(error => console.error(error))
+  // res.render('index',{
+  //   restaurants: restaurantList.results
+  // })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurantDetail = restaurantList.results.find( item => item.id.toString() === req.params.restaurant_id)
+  const id = req.params.restaurant_id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show',{restaurant}))
+    .catch(error => console.error(error))
+  // const restaurantDetail = restaurantList.results.find( item => item.id.toString() === req.params.restaurant_id)
   
-  res.render('show', {
-    restaurant: restaurantDetail
-  })
+  // res.render('show', {
+  //   restaurant: restaurantDetail
+  // })
 })
 app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const filteredRestaurants = restaurants.filter(item => 
+         item.name.toLowerCase().includes(keyword) ||
+         item.category.toLowerCase().includes(keyword)
+      )
+      if (filteredRestaurants.length !== 0){
+        res.render('index', {
+          restaurants: filteredRestaurants,
+          keyword: req.query.keyword
+        })
+      }else{
+        res.render('error', {
+          keyword: req.query.keyword
+        })
+      }
   
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(item => {
-    const restaurantsName = item.name.toLowerCase().includes(keyword.toLowerCase().trim())
-    const restaurantsCategory = item.category.toLowerCase().includes(keyword.toLowerCase().trim())
-    return restaurantsName || restaurantsCategory
-  })
-  if (restaurants.length !== 0) {
-    res.render('index', {
-          restaurants: restaurants,
-          keyword: keyword
+    
+      
     })
-  }else{
-    res.render('error', {
-      keyword: keyword
-    })
-  }
+  // const restaurants = restaurantList.results.filter(item => {
+  //   const restaurantsName = item.name.toLowerCase().includes(keyword)
+  //   const restaurantsCategory = item.category.toLowerCase().includes(keyword)
+  //   return restaurantsName || restaurantsCategory
+  // })
+  // if (restaurants.length !== 0) {
+  //   res.render('index', {
+  //         restaurants: restaurants,
+  //         keyword: keyword
+  //   })
+  // }else{
+  //   res.render('error', {
+  //     keyword: keyword
+  //   })
+  // }
   
 })
 app.listen(port, () => {
