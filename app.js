49f826d3a0f709f2275a73f72e1c4819fd546ae6
TCY1020 <a25023423@gmail.com>
2023-05-10 02:28:09 +0800

    feat: restaurants.json upload to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurants.json')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const port = 3000
+
+// mongoDB連線
+mongoose.connect(process.env.MONGODB_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('erro', () =>{
+  console.log('mongodb error')
+})
+db.once('open', () =>{
+  console.log('mongodb connected!')
+})
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
-app.use(express.static('public'))
+app.use(express.static('public'), bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index',{
