b3ccaf63f6dcc0b648175a16c8172b2b68743f27
TCY1020 <a25023423@gmail.com>
2023-05-10 11:13:36 +0800

    feat: Delete,Edit function

--- app.js
+++ app.js
@@ -34,14 +34,16 @@ app.get('/', (req, res) => {
   .catch(error => console.error(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const id = req.params.restaurant_id
+// 餐廳細節
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
   Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('show',{restaurant}))
     .catch(error => console.error(error))
 })
 
+// 新增餐廳
 app.get('/restaurant/new',(req, res) =>{
   res.render('new')
 })
@@ -52,6 +54,31 @@ app.post('/restaurants',(req, res) =>{
     .catch(error => console.log(error))
 })
 
+// 編輯餐廳
+app.get('/restaurants/:id/edit', (req, res) =>{
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', {restaurant}))
+    .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/edit', (req, res) =>{
+  const id = req.params.id
+  const restaurantEdit = req.body
+  Restaurant.findByIdAndUpdate(id, restaurantEdit)
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+//刪除餐廳
+ app.post('/restaurants/:id/delete', (req, res) =>{
+  const id =req.params.id
+  Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+}) 
+// 搜尋餐廳
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
   Restaurant.find()
