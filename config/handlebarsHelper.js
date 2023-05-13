6ca0c4264035ed39fbd8e1bba7b87cb8b2b4a55d
TCY1020 <a25023423@gmail.com>
2023-05-13 22:27:45 +0800

    code refactoring Router

--- config/handlebarsHelper.js
+++ config/handlebarsHelper.js
@@ -1,11 +1,9 @@
-
-//TODO:handlebars-helper筆記
 const Handlebars = require('handlebars')
 // choose: helper名稱，a,b為可傳入變數
 Handlebars.registerHelper('choose', (a, b, option) => {
   if(a === b){
-    return option.fn(this)// handlebars內文字，條件成立時出現
+    return option.fn(this)// 條件成立時出現，handlebars內文字，
   }
-    return option.inverse(this)// handlebars內文字，條件不成立時出現
+  return option.inverse(this)// 條件不成立時出現，handlebars內文字
   
 })
